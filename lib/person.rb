0443e8e89955162a64c40de15b3e7490e4f4334a
iwilliams83 <indirajamurath@gmail.com>
2017-12-14 21:50:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -23,11 +23,11 @@ end
 
 #When your tests pertaining to the first code along are passing, un-comment out the below!
 
-#beyonce = Person.new("Beyonce")
-#jayz = Person.new("Jay-Z")
-# beyonce.go_on_a_date(jayz, true)
-# puts beyonce.happiness
-# puts jayz.happiness
+beyonce = Person.new("Beyonce")
+jayz = Person.new("Jay-Z")
+beyonce.go_on_a_date(jayz, true)
+puts beyonce.happiness
+puts jayz.happiness
 
 #When your tests pertaining to the second code along are passing, un-comment out the below!
 
